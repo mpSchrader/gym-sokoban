f63c813c63cc7b955516131c0b4123a62d7ad1d4
mpSchrader <maxphilipp.schrader@hotmail.de>
2018-03-14 15:07:41 +0100

    Gifs

--- test_env.py
+++ test_env.py
@@ -2,11 +2,14 @@ import gym
 import gym_sokoban
 from gym_sokoban.envs.room_utils import ACTION_LOOKUP
 import time
-
+from scipy.misc import imsave
+ts = time.time()
 env = gym.make('Sokoban-v0')
 #env = gym.make('CartPole-v0')
 for i_episode in range(1):#20
     observation = env.reset()
+    observation = env.render(mode='rgb_array')
+    imsave('docs/run_{}_step_{}.png'.format(ts, -1), observation)
     for t in range(100):#100
         env.render()#mode='rgb_array')
         time.sleep(1)
@@ -19,6 +22,7 @@ for i_episode in range(1):#20
         except:
             action = 0
         observation, reward, done, info = env.step(action)
+        imsave('docs/run_{}_step_{}.png'.format(ts, t), observation)
         print(ACTION_LOOKUP[action], reward, done, info)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
