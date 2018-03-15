db55fe387c9a119d3143c96c76d0a0d87a5aa267
mpSchrader <maxphilipp.schrader@hotmail.de>
2018-03-15 12:06:41 +0100

    Add version constraints

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_sokoban',
       version='0.0.1',
-      install_requires=['gym>=0.2.3', 'scipy', 'numpy']
+      install_requires=['gym>=0.2.3', 'scipy>=1.0.0', 'numpy>=1.14.1']
 )
