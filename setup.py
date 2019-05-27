df999bb63276522bf572e85285b80c5a0acd995d
mpSchrader <maxphilipp.schrader@hotmail.de>
2019-05-27 15:58:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 
-setup(name='gym_sokoban',
-      version='0.1.0',
+setup(
+      name='gym_sokoban',
+      version='0.2.0',
+      description='Sokoban environment for OpenAI Gym',
       install_requires=['gym>=0.2.3', 'scipy>=1.0.0', 'numpy>=1.14.1']
 )
